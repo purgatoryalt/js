cdc0b7522c2f45f0bd7d394d38563329121e8f00
purgatoryalt <102757720+purgatoryalt@users.noreply.github.com>
2022-05-05 13:35:44 -0300

    Add files via upload

---
+++ exercicio2.js
@@ -0,0 +1,34 @@
+//1-a
+
+console.log(10+20*30);
+
+//1-b
+
+console.log(4**2/30);
+
+//1-c 
+
+console.log((9**4 +2)* 6-1)
+
+// 2-a
+
+console.log(10 % 3 * 10 ** 1 + 1 - 10 * 4 / 2)
+
+//3
+const nome='Leonardo Cardoso';
+console.log(nome);
+
+//4
+const g=3;
+const f=5;
+
+console.log(2*g*3*f);
+//5
+const a=3;
+const b=4;
+const c=6;
+console.log(a+b+c)
+//6
+const s=1800;
+console.log(s*2);
+
---
+++ primeiro.js
@@ -0,0 +1,8 @@
+const nome ='Leonardo';
+console.log(nome)
+
+
+const a=1500;
+const b=1200;
+
+console.log(a+b);
