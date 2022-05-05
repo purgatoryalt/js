adff7645ac12415553854ae78d02ae955efb65bc
purgatoryalt <102757720+purgatoryalt@users.noreply.github.com>
2022-05-05 15:42:28 -0300

    Add files via upload

---
+++ exercicio3.js
@@ -0,0 +1,43 @@
+//1-
+
+function nome(l){
+ console.log(l)
+ return
+}
+
+nome('Leonardo')
+
+
+//2-
+
+function soma (x,y){
+    let res =x+y
+    console.log(res)
+    return
+}
+
+soma(25,5);
+
+// 3-
+
+function multiplicação(a,b){
+    console.log(a*b)
+    return;
+}
+
+multiplicação(15,2)
+
+//4-
+
+function media (e,f,g,h){
+    let x = (e+f+g+h)/4;
+    console.log(x);
+    return
+}
+
+media(10,5,6,5);
+
+
+
+
+
